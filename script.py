0b26adeeb230408ae62e7d3efa71f5724b2b907e
SNIPRS <taozhang1917@gmail.com>
2022-07-15 13:03:23 -0400

    H2

--- script.py
+++ script.py
@@ -56,52 +56,59 @@ def Hs_sum_costs(SHs, Shs, icosts):
             Hs_s.append(H); hs_s.append(h), costs.append(icost)
     return np.array(Hs_s), np.array(hs_s), costs
 
-n, Hm, Hs, hs, Ps = LiH_hamiltonian()
+geometry = [("H", (0.0, 0.0, 0.0)), ("H", (0.0, 0.0, 1))]
+basis = "sto-3g"
+multiplicity = 1
+charge = 0
+
+n, Hm, Hs, hs, Ps = Paulis_from_Molecule(None, geometry, basis, multiplicity, charge)
+
+
 
 terms = list(zip(Hs, hs, Ps))
 terms1 = [list(term) for term in terms]
 terms = np.array(terms1, dtype=object)
-terms = terms[np.abs(terms[:, 1]).argsort()]
-
-
 
 # Reorder 
-terms[[16,22]] = terms[[22,16]]
-terms[[17,23]] = terms[[23,17]]
-terms[[18,20]] = terms[[20,18]]
-terms[[19,21]] = terms[[21,19]]
-
-# XY
-terms[5][:2], terms[6][:2] = -terms[5][:2], -terms[6][:2]
-terms[5][2], terms[6][2] = '-'+terms[5][2], '-'+terms[6][2]
-
+terms[[5,9]] = terms[[9,5]]
+terms[[10,14]] = terms[[14,10]]
+terms[[9,10]] = terms[[10,9]]
 
 # Z
-terms[22][:2], terms[23][:2] = -terms[22][:2], -terms[23][:2]
-terms[22][2], terms[23][2] = '-'+terms[22][2], '-'+terms[23][2]
+terms[3][:2], terms[4][:2] = -terms[3][:2], -terms[4][:2]
+terms[3][2], terms[4][2] = '-'+terms[3][2], '-'+terms[4][2]
+# terms[1:5, 1] = np.mean(terms[1:5, 1])
 
-def get_clique(i, j):
-    return np.sum(np.stack(terms[i:j][:, 0]), axis=0)
+# XY
+terms[7][:2], terms[8][:2] = -terms[7][:2], -terms[8][:2]
+terms[7][2], terms[8][2] = '-'+terms[7][2], '-'+terms[8][2]
+
+# ZZ
+# terms[9:13, 1] = np.mean(terms[9:13, 1])
 
 for i, term in enumerate(terms):
     print(i, term[1:])
 
-groups = [(0,4), (4,8), (8,12), 
-          (12,16), (16,20), (16,17),
-         (17,18), (20,22), (22,24),
-         (24,26)]
-hs_s = np.array([terms[0][1], terms[4][1], terms[8][1],
-         terms[12][1], terms[18][1], terms[16][1]-terms[18][1],
-         terms[17][1]-terms[18][1], terms[20][1], terms[22][1],
-         terms[24][1]])
-Hs_s = np.array([get_clique(group[0], group[1]) for group in groups])
-icosts = np.array([[2,3], [1,3], [2,3], 
-                   [2,3], [1,1], [1,0], 
-                   [1,0], [1,2], [1,0],
-                   [1,0]])
+def get_clique(i, j):
+    return np.sum(np.stack(terms[i:j][:, 0]), axis=0)
 
-# Get rid of first identity
+SI = get_clique(0,1)
+SZ0 = get_clique(1,3)
+SZ1 = get_clique(1,5)
+SXY = get_clique(5,9)
+SZZ0 = get_clique(9,10)
+SZZ1 = get_clique(10,11)
+SZZ2 = get_clique(9,13)
+SZZ3 = get_clique(13,15)
+
+Hs_s = np.array([SZ0, SZ1, SXY, SZZ0, SZZ1, SZZ2, SZZ3])
+hs_s = np.array([terms[1][1]-terms[3][1], terms[3][1], terms[5][1], terms[9][1]-terms[11][1], terms[10][1]-terms[11][1], terms[11][1], terms[13][1]])
+icosts = np.array([[2,2], [2,2], [1,3], [1,1], [1,1], [1,1], [1,2]])
 Hs, hs = Hs[1:], hs[1:]
+
+# Hs_s, hs_s, icosts = Hs_sum_costs(SHs, Shs, icosts)
+
+
 
 # Start simulation
 t = 1
@@ -112,8 +119,8 @@ Ns = [2**i + 10 for i in range(5, M)]
 st = time.time()
 
 # print(time.time()-st)
-errors_costs = np.array([Error_cost(Hm, Hs_s, hs_s, t, rho, N, icosts, M=5000, threads=32) for N in Ns])
-errors_costs1 = np.array([Error_cost(Hm, Hs, hs, t, rho, N, M=5000, threads=32) for N in Ns])
+errors_costs = np.array([Error_cost(Hm, Hs_s, hs_s, t, rho, N, icosts, M=1000, threads=48) for N in Ns])
+errors_costs1 = np.array([Error_cost(Hm, Hs, hs, t, rho, N, M=1000, threads=48) for N in Ns])
 errors, errors1 = errors_costs[:, 0], errors_costs1[:, 0]
 tcosts, rcosts = errors_costs[:, 1], errors_costs[:, 2]
 
